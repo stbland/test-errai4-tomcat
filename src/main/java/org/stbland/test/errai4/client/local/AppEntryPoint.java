731d7fbead069de492009419902002592cbf755b
Stéphane BEAUFORT <stbland@gmail.com>
2017-02-03 16:31:58 +0100

    test sending message to server with the Errai Client Bus

--- src/main/java/org/stbland/test/errai4/client/local/AppEntryPoint.java
+++ src/main/java/org/stbland/test/errai4/client/local/AppEntryPoint.java
@@ -1,16 +1,82 @@
 package org.stbland.test.errai4.client.local;
 
-import com.google.gwt.user.client.Window;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
 import javax.annotation.PostConstruct;
+import org.jboss.errai.bus.client.api.base.MessageBuilder;
+import org.jboss.errai.bus.client.api.messaging.Message;
+import org.jboss.errai.bus.client.api.messaging.MessageBus;
+import org.jboss.errai.bus.client.api.messaging.MessageCallback;
+import org.jboss.errai.common.client.protocols.MessageParts;
 import org.jboss.errai.ioc.client.api.EntryPoint;
 
 @EntryPoint
 public class AppEntryPoint {
 
+    @Inject
+    private MessageBus messageBus;
+
+    private final Label statusLabel = new Label();
+    private final TextBox messageTextBox = new TextBox();
+
     @PostConstruct
     public void onLoad() {
-
-        Window.alert("Errai 4 works on Tomcat server");
-
+        final Widget view = buildView();
+        RootPanel.get().add(view);
     }
+
+    private Widget buildView() {
+        final Button sendMessageButton = new Button("Send");
+
+        final Panel view = new VerticalPanel();
+        view.add(new Label("Message:"));
+        view.add(messageTextBox);
+        view.add(sendMessageButton);
+        view.add(new Label("Status:"));
+        view.add(statusLabel);
+
+        sendMessageButton.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                sendMessage();
+            }
+        }
+        );
+
+        return view;
+    }
+
+    private void sendMessage() {
+        final String messageText = messageTextBox.getText();
+        if ("".equals(messageText)) {
+            setStatus("message is mandatory");
+            messageTextBox.setFocus(true);
+            return;
+        }
+        setStatus("sending message ...");
+
+        MessageBuilder.createMessage()
+                .toSubject("HelloWorldService")
+                .withValue(messageText)
+                .done()
+                .repliesTo(new MessageCallback() {
+                    public void callback(Message message) {
+                        setStatus("Response from server: " + message.get(String.class, MessageParts.Value));
+                    }
+                })
+                .sendNowWith(messageBus);
+    }
+
+    private void setStatus(String status) {
+        statusLabel.setText(status);
+    }
+
 }
---
+++ src/main/java/org/stbland/test/errai4/server/services/HelloWorldService.java
@@ -0,0 +1,29 @@
+package org.stbland.test.errai4.server.services;
+
+import org.jboss.errai.bus.client.api.base.MessageBuilder;
+import org.jboss.errai.bus.client.api.messaging.Message;
+import org.jboss.errai.bus.client.api.messaging.MessageCallback;
+import org.jboss.errai.bus.server.annotations.Service;
+import org.jboss.errai.common.client.protocols.MessageParts;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Service
+public class HelloWorldService implements MessageCallback {
+
+    final Logger logger = LoggerFactory.getLogger(HelloWorldService.class);
+
+    @Override
+    public void callback(Message message) {
+        final String messageText = message.get(String.class, MessageParts.Value);
+        logger.debug("HelloWorldService.callback(message) messageText: " + messageText);
+
+        MessageBuilder.createConversation(message)
+                .subjectProvided()
+                .withValue("message received: " + messageText)
+                .done().reply();
+
+    }
+
+}
